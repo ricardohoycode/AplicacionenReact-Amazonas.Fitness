39661e6e47edaf996f734eb4a593f09fd4878656
Ricardo Martin Hoyos B <94667856+ricardohoycode@users.noreply.github.com>
2022-04-08 14:44:46 -0800

    Delete src directory

--- src/services/users.service.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = 'https://jsonplaceholder.typicode.com/users'
-
-export const getUsers = () => {
-    return fetch(baseUrl)
-}
-
-export const getUser = (id) => {
-    return fetch(`${baseUrl}/${id}`)
-}
